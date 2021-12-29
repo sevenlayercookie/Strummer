d9555c8d239203c6f58d29e5a2b50f0a2876f346
Harrison Rose <sevenlayercookie@gmail.com>
2021-12-29 13:13:02 -0500

    Initial Commit

---
+++ Strummer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Strummer
+//
+//  Created by Rebekah Rose on 12/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
